8083193e8f4b8cc28a29d5d58faa5deeaea957f5
Kimaya Abhyankar <kimayanabhyankar@gmail.com>
2022-11-10 03:34:01 +0530

    Create expressions.c

---
+++ expressions.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+        int a, b;
+        printf("Enter 2 numbers:\na = ");
+        scanf("%d", &a);
+        printf("\nb = ");
+        scanf("%d", &b);
+        printf("\nAddition of a and b is: %d\n", a+b);
+        printf("\nSubtraction of a and b is: %d\n", a-b);
+        printf("\nMultiplication of a and b is: %d\n", a*b);
+        printf("\nDivision of a and b is: %d\n", a/b);
+        return 0;
+}
