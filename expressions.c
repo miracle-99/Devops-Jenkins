6fdf62b96b067dfb1c87328e098c34bc1a2d3d3b
Kimaya Abhyankar <kimayanabhyankar@gmail.com>
2022-11-10 12:33:51 +0530

    Update expressions.c

--- expressions.c
+++ expressions.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
 
 int main() {
-        int a, b;
-        printf("Enter 2 numbers:\na = ");
-        scanf("%d", &a);
-        printf("\nb = ");
-        scanf("%d", &b);
+        int a = 341, b = 87;
+        printf("2 numbers are a = %d and b = %d\n", a, b);
         printf("\nAddition of a and b is: %d\n", a+b);
         printf("\nSubtraction of a and b is: %d\n", a-b);
         printf("\nMultiplication of a and b is: %d\n", a*b);
