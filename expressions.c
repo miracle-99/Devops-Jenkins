f98de7252a639512a13e2dfdb6bee6c446aaa331
Kimaya Abhyankar <kimayanabhyankar@gmail.com>
2022-11-10 13:39:42 +0530

    Update expressions.c

--- expressions.c
+++ expressions.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-        int a = 3323, b = 232;
+        int a = 3321, b = 232;
         printf("2 numbers are a = %d and b = %d\n", a, b);
         printf("\nAddition of a and b is: %d\n", a+b);
         printf("\nSubtraction of a and b is: %d\n", a-b);
